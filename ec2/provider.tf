66b9b8bc4ad7c0c63c1ceb286b9a581455d8e3e2
GattamPavan <pavanreddy2266@gmail.com>
2026-05-17 17:15:44 +0530

    terraform

--- ec2/provider.tf
+++ ec2/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "6.33.0" #terraform AWS provider version
+      source = "hashicorp/aws"
+      version = "6.33.0" # Terraform AWS provider version
     }
   }
 }
